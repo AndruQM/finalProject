0e6ae1a5403ed059372672bda1da492d400cf140
AndruQM <quispemoyaa@gmail.com>
2024-08-15 10:29:35 -0400

    creacion de proyecto

---
+++ learn-c-sharp-6-Verbs/API/Startup.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Context;
+using DataAccess.Repositories;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Services;
+using Services.Interfaces;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            RegisterTypes(services);
+
+            services.AddDbContext<APIContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("APIDatabase"), b => b.MigrationsAssembly("API"));
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseRouting();
+                app.UseAuthentication();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("Todo Bien");
+                    });
+                });
+            }
+        }
+
+        public static void RegisterTypes(IServiceCollection services) 
+        {
+            //Contection beetween EntityFramework and Project Context
+            services.AddTransient<DbContext, APIContext>();
+
+            //Connection for Employee
+            services.AddTransient<IEmployeeService, EmployeeService>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+        }
+    }
+}
